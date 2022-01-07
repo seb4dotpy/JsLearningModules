7ad40708eeedb08ff9a26c417a7164e38e39aeeb
SebastianOhberg <sebastian.ohberg01@gmail.com>
2022-01-07 01:11:25 -0300

    Sending new branch

---
+++ FiguresModule/triangles.js
@@ -0,0 +1,11 @@
+const sideA = 5; const sideB = 15; const sideC = 32;
+
+/*
+    Triangle types
+    equilateral = Al sides equal
+    isosceles = Two sides equal
+    scalene = No side equal
+*/
+function sideComparison(sideA, sideB, sideB){
+
+}
